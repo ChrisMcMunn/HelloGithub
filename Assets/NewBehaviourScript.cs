e1c033b84b15f41ae01ec5a1ec56e321614ca188
GAD-ChrisMcMunn <63013022+ChrisMcMunn@users.noreply.github.com>
2021-01-14 10:59:11 +0000

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,12 +7,42 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+         * (Anakin Skywalker and Space Jesus have their climactic battle on Mustafar. Obi-Wan then jumps out of the lava raft)
+         
+        Obi - Wan: It's over, Anakin! I have the high ground!
+
+        Anakin: You underestimate my power.
+
+        Obi - Wan: Don't try it. I have the High ground!
+
+        (Anakin angrily attempts to jump out of the raft and hit Obi - Wan on the high ground, only to get his legs and left arm chopped off.Anakin then drops his lightsaber, rolls of the high ground and lands near the lava.)
+
+        Obi - Wan: You were supposed to destroy the Sith, not join them! I still have the High ground!
+
+        Anakin: I hate you and your high ground!
+
+        Obi - Wan: You were my brother, Anakin!I love you, along with the High ground!
+
+         (Anakin catches fire and screams.)
+
+        Obi - Wan: Whoops.I better grab that there. (grabs Anakin's lightsaber while on the high ground) High ground.
+
+         Anakin: Wait.Are you f * cking robbing me ?
+
+        Obi - Wan : I'll give it to your son someday if Padme isn't, you know, dead.I mean, she looked pretty dead to me, from up here... on the high ground.
+
+         Anakin: (with a COMPLETELY scarred face) Wait, wait, wait, wait, wait.Uh, I'm literally just c-cooking to death. Please just kill me.
+
+        Obi - Wan: That's not the high ground wa-- I mean, the Jedi way. You know what I'm saying. I'm saying I've beaten you, and I'm on the high ground, so... High ground.
+
+        Some - Rando: Props to whoever made this, this made my day.high ground!~
+            */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
